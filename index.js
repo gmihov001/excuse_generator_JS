86b8e241a9a78e60056b1ea398915497fb454ca2
gmihov001 <gmihov001@gmail.com>
2019-08-20 00:47:34 +0000

    Initial

---
+++ index.js
@@ -0,0 +1,25 @@
+console.log("HELLO WORLD");
+
+let who = ['the dog','my granma','his turtle','my bird'];
+let what = ['eat','pissed','crushed','broked'];
+let when = ['before the class','right in time','when I finished','during my lunch','while I was praying'];
+
+let excuse1 = who[Math.floor(Math.random() * who.length)] + " " + what[Math.floor(Math.random() * what.length)] + " " + when[Math.floor(Math.random() * when.length)] ;
+
+let excuse2 = who[Math.floor(Math.random() * who.length)] + " " + what[Math.floor(Math.random() * what.length)] + " " + when[Math.floor(Math.random() * when.length)] ;
+
+let excuse3 = who[Math.floor(Math.random() * who.length)] + " " + what[Math.floor(Math.random() * what.length)] + " " + when[Math.floor(Math.random() * when.length)] ;
+
+let excuse4 = who[Math.floor(Math.random() * who.length)] + " " + what[Math.floor(Math.random() * what.length)] + " " + when[Math.floor(Math.random() * when.length)] ;
+
+let excuse5 = who[Math.floor(Math.random() * who.length)] + " " + what[Math.floor(Math.random() * what.length)] + " " + when[Math.floor(Math.random() * when.length)] ;
+
+console.log(excuse1);
+
+console.log(excuse2);
+
+console.log(excuse3);
+
+console.log(excuse4);
+
+console.log(excuse5);
